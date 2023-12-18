a547bafa2009f03f6ed215d36bf125fcca7a7826
Aditya Anand <20bec004@iiitdmj.ac.in>
2023-12-19 00:35:37 +0530

    fix: backend and client

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const app = express()
-const port = 5000
 require('dotenv').config({path:'.env'});
 const mongoDB = require("./db")
 mongoDB();
 
+const port = 5000
 const user = require("./routes/user");
 const courses = require("./routes/courses");
 
